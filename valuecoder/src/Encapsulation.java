e2d80c10bf0cdf0fef5cb232640b4fada1607151
Rishabh <Rishabh@Thunder>
2021-05-26 10:28:32 +0530

    Encapsulation

---
+++ valuecoder/src/Encapsulation.java
@@ -0,0 +1,23 @@
+class EmployementCount
+{
+	private int numOfEmployee=0;
+	public void setOfEmployee(int count)
+	{
+		numOfEmployee=count;
+	}
+	public double getNoOfEmployee() 
+	{
+		return numOfEmployee;
+	}
+}
+
+public class Encapsulation
+{
+	public static void main(String[] args) 
+	{
+		EmployementCount ec=new EmployementCount();
+		ec.setOfEmployee(1013);
+		System.out.println((int)ec.getNoOfEmployee());
+	}
+
+}
