cdbd2597387a216569b26a2201c089e506975513
Rishabh <Rishabh@Thunder>
2021-05-26 10:15:51 +0530

    Singleton_class

---
+++ valuecoder/src/Test.java
@@ -0,0 +1,20 @@
+class SingletonClass 
+{
+	static SingletonClass sc=new SingletonClass();
+	private SingletonClass()
+	{
+		
+	}
+	public static SingletonClass getInstance()
+	{
+		System.out.println("created instance !!");
+		return sc;
+	}
+}
+public class Test {
+
+	public static void main(String[] args) 
+	{
+		SingletonClass sc=SingletonClass.getInstance();
+	}
+}
