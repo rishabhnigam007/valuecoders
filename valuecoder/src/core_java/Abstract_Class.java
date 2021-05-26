c9fbdbcd1f11f645ce9c26208703f9aa75563d80
Rishabh <Rishabh@Thunder>
2021-05-26 10:38:16 +0530

    Abstract_Class

---
+++ valuecoder/src/core_java/Abstract_Class.java
@@ -0,0 +1,20 @@
+package core_java;
+
+abstract class Person
+{
+	abstract void eat();
+}
+
+public class Abstract_Class 
+{
+	public static void main(String[] args) 
+	{
+		Person p = new Person() {
+			void eat()
+			{
+				System.out.println("eating !!");
+			}
+		};
+		p.eat();
+	}
+}
