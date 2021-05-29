9763df45a20e27f7516e78b2718ad6faca4cc87c
Rishabh <Rishabh@Thunder>
2021-05-29 11:33:26 +0530

    FileReader_Class

---
+++ valuecoder/src/file_handling/FileReader_Class.java
@@ -0,0 +1,38 @@
+package file_handling;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReader_Class 
+{
+	public static void main(String[] args) throws IOException 
+	{
+		File f= new File("C:\\Users\\Rishabh\\git\\valuecoders\\valuecoder\\src\\file_handling\\FileWriter_Class.txt");
+		
+		FileReader fr=null;
+		
+		try 
+		{
+			fr=new FileReader(f);
+			int data=fr.read();
+			while(data != -1)
+			{
+				System.out.print((char)data);
+				data=fr.read();
+			}
+			System.out.println();
+			
+		}
+		catch (FileNotFoundException e) 
+		{
+			System.out.println("File not exists");
+			System.out.println(e.getMessage());
+		}
+		finally
+		{
+			fr.close();
+		}
+	}
+}
